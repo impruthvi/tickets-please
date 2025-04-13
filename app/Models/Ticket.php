0e28fb2f938f489a6e1b9af3d9f191a735ee6704
impruthvi <pruthvirajput97@gmail.com>
2025-04-13 10:57:32 +0530

    feat: Add UserController and user request validation, and update routes for user resource

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
@@ -15,4 +16,9 @@ class Ticket extends Model
         'description',
         'status'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
