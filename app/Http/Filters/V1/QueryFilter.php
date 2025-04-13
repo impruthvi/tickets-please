f83ca4fdc893d5ef063583e54a830c2092bf6e68
impruthvi <pruthvirajput97@gmail.com>
2025-04-13 23:32:31 +0530

    refactor: Simplify apply method in QueryFilter and enhance sorting functionality

--- app/Http/Filters/V1/QueryFilter.php
+++ app/Http/Filters/V1/QueryFilter.php
@@ -9,14 +9,32 @@ abstract class QueryFilter
 {
     protected $builder;
     protected $request;
+    protected $sortable = [];
 
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
+
+
+    public function apply(Builder $builder)
+    {
+
+        $this->builder = $builder;
+
+        foreach ($this->request->all() as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
     protected function filter($arr)
     {
+
         foreach ($arr as $key => $value) {
             if (method_exists($this, $key)) {
                 $this->$key($value);
@@ -26,10 +44,29 @@ abstract class QueryFilter
         return $this->builder;
     }
 
-    public function apply(Builder $builder)
+    protected function sort($value)
     {
-        $this->builder = $builder;
+        $sortAttributes = explode(',', $value);
 
-        $this->filter($this->request->all());
+        foreach ($sortAttributes as $sortAttribute) {
+            $direction = 'asc';
+
+            if (strpos($sortAttribute, '-') === 0) {
+                $direction = 'desc';
+                $sortAttribute = substr($sortAttribute, 1);
+            }
+
+            if (!in_array($sortAttribute, $this->sortable) && !array_key_exists($sortAttribute, $this->sortable)) {
+                continue;
+            }
+
+            $columnName = $this->sortable[$sortAttribute] ?? null;
+
+            if ($columnName === null) {
+                $columnName = $sortAttribute;
+            }
+
+            $this->builder->orderBy($columnName, $direction);
+        }
     }
 }
