4ff70ab8ae75597c6b3225ba60e1bf80e0947974
impruthvi <pruthvirajput97@gmail.com>
2025-04-14 23:32:48 +0530

    refactor: streamline API exception handling and improve response formatting

--- app/Exceptions/ApiExceptions.php
+++ app/Exceptions/ApiExceptions.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use App\Traits\ApiResponses;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
@@ -13,6 +14,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ApiExceptions extends Exception
 {
+    use ApiResponses;
 
     public static array $handlers = [
         AuthenticationException::class => 'handleAuthenticationException',
@@ -23,24 +25,22 @@ class ApiExceptions extends Exception
 
     public static function handleAuthenticationException(AuthenticationException $e, Request $request): JsonResponse
     {
-
         // log that sensitive stuff
         // should move this out to custom logger
         $source = 'Line: ' . $e->getLine() . ', File: ' . $e->getFile();
         Log::notice(basename(get_class($e)) . ' - ' . $e->getMessage() . ' - ' . $source);
 
-        return response()->json([
-            'error' => [
-                'type' => basename(get_class($e)),
-                'status' => 401,
-                'message' => $e->getMessage()
-            ]
-        ]);
+        return (new self)->error([
+            'status' => 401,
+            'message' => $e->getMessage(),
+            'type' => basename(get_class($e))
+        ], 401);
     }
 
     public static function handleValidationException(ValidationException $e, Request $request): JsonResponse
     {
-        foreach ($e->errors() as $key => $value)
+        $errors = [];
+        foreach ($e->errors() as $key => $value) {
             foreach ($value as $message) {
                 $errors[] = [
                     'type' => basename(get_class($e)),
@@ -48,20 +48,17 @@ class ApiExceptions extends Exception
                     'message' => $message,
                 ];
             }
+        }
 
-        return response()->json([
-            'errors' => $errors
-        ]);
+        return (new self)->error($errors, 422);
     }
 
     public static function handleNotFoundException(ModelNotFoundException|NotFoundHttpException $e, Request $request): JsonResponse
     {
-        return response()->json([
-            'error' => [
-                'type' => basename(get_class($e)),
-                'status' => 404,
-                'message' => 'Not Found ' . $request->getRequestUri()
-            ]
-        ]);
+        return (new self)->error([
+            'status' => 404,
+            'message' => 'Not Found ' . $request->getRequestUri(),
+            'type' => basename(get_class($e))
+        ], 404);
     }
 }
